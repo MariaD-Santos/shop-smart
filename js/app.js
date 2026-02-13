d5a0e7e2b780924492e1a59235db10d480ee360f
MariaD-Santos <maria.j.santos108@aluno.senai.br>
2026-02-13 14:57:02 -0300

    finalizado o trabalho!

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const input02 = document.getElementById('valor02');
 
 // pegando também o resultado já que vai ser o mesmo para as funções.
 const resultado = document.getElementById('resultado');
-
+resultado.style.display = 'none'
 function CalcularTotal(){
     const v1 = parseFloat(input01.value);
     const v2 = parseFloat(input02.value);
@@ -19,30 +19,106 @@ function CalcularTotal(){
         resultado.style.display = 'flex';
 
         resultado.innerHTML =
-            ` <h2>seu Total:<h2/> 
-                <ul>
+            ` <h2>Seu total:<h2/> 
+                
                     <p>você adquiriu ${v2} unidades com o valor unitário de ${v1}<p/>
-                    <li>O total de sua compra foi de: ${v1*v2}<li/>
-                <ul/>
+                    <p>O total de sua compra foi de: R$${v1*v2}<p/>
+               
             `
             //fazer o p ao final de tudo
     }
 };
 
 function Desconto() {
-    
+    const v1 = parseFloat(input01.value);
+    const v2 = parseFloat(input02.value);
+
+    // verificando se é um número ou não
+    if (isNaN(v1) || isNaN(v2)) {
+        alert('Valor01 ou Valor02 inválido. Por favor digite um número')
+    }
+
+    else {
+        resultado.style.display = 'flex';
+        let desconto = v1*v2 / 100
+        resultado.innerHTML =
+            ` <h2>Desconto aplicado!<h2/> 
+                
+                <p>foi aplicado a sua compra um desconto de ${v2}% sobre o valor de ${v1}<p/>
+                <p>O valor final do desconto foi: R$${v1- desconto}<p/>
+              
+            `
+            //fazer o p ao final de tudo
+    }
 };
 
 function Juros() {
+    const v1 = parseFloat(input01.value);
+    const v2 = parseFloat(input02.value);
 
+    // verificando se é um número ou não
+    if (isNaN(v1) || isNaN(v2)) {
+        alert('Valor01 ou Valor02 inválido. Por favor digite um número')
+    }
+
+    else {
+        resultado.style.display = 'flex';
+        let acrescimo = v1 *v2 / 100
+        resultado.innerHTML =
+            ` <h2>Acréscimo aplicado a compra<h2/> 
+                
+                    <p>Foi aplicado um acréscimo de ${v2}% sobre o valor de: ${v1}<p/>
+                    <p>O valor final com os juros foi de: R$${v1 + acrescimo}<p/>
+           
+            `
+            //fazer o p ao final de tudo
+    }
 };
 
 function Comissao() {
+    const v1 = parseFloat(input01.value);
+    const v2 = parseFloat(input02.value);
 
+    // verificando se é um número ou não
+    if (isNaN(v1) || isNaN(v2)) {
+        alert('Valor01 ou Valor02 inválido. Por favor digite um número')
+    }
+
+    else {
+        resultado.style.display = 'flex';
+        let comissao = v2 / 100
+        resultado.innerHTML =
+            ` <h2>Sua comissão:<h2/> 
+              
+                    <p>Comissão de ${v2}% sobre uma venda de ${v1}<p/>
+                    <p>O valor foi de: R$${comissao*v1}<p/>
+             
+            `
+            //fazer o p ao final de tudo
+    }
 };
 
 function Lucro() {
+    const v1 = parseFloat(input01.value);
+    const v2 = parseFloat(input02.value);
 
+    // verificando se é um número ou não
+    if (isNaN(v1) || isNaN(v2)) {
+        alert('Valor01 ou Valor02 inválido. Por favor digite um número')
+    }
+
+    else {
+        resultado.style.display = 'flex';
+
+        resultado.innerHTML =
+            ` <h2>Seu Lucro:<h2/> 
+              
+                    <p>Preço de sua venda ${v1}|Custo: ${v2}<p/>
+                    <p>O total recebido da venda foi de: R$${v1-v2}<p/>
+        
+            `
+            //fazer o p ao final de tudo
+    }
 };
 
 function Limpar() {
